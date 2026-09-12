519f8f1a2008f6aef366893442f642c9d33048ba
shamsu082277 <shamsu082277@gmail.com>
2026-09-12 22:21:56 +0600

    Hero Section Update

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,29 @@
+const HeroSection = () => {
+    return (
+        <div className="container mx-auto py-10">
+            <div className="min-h-screen">
+                <div className="hero-content flex-col lg:flex-row">
+                    
+                    <div>
+                        <h1 className="text-5xl font-bold ">Build Your Ideal <br></br><span className="hero-text-gradient-color">Development Stack</span> </h1>
+                        <p className="py-6">
+                            Explore frontend, backend, database, and tooling options,
+                            compare them side by side, and put together the stack that fits your
+                            next project.
+                        </p>
+                        <div className="flex gap-4">
+                            <button className="btn btn-primary py-5 min-w-48 rounded-lg explore-btn">Explore Technologies</button>
+                            <button className="btn py-5 rounded-lg min-w-48"> Learn More</button>
+                        </div>
+                    </div>
+                    <img
+                        alt="Tailwind CSS hero component"
+                        src="/src/assets/banner-stack.png"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
     return (
-        <div className="bg-base-100 shadow-sm">
+        <div className="bg-base-100 shadow-sm sticky top-0 z-50">
             <div className="navbar container mx-auto ">
             <div className="navbar-start">
                 <div className="dropdown">
